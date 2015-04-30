539c4d9afdd2024bed5b1e74ebde154a0b81a415
waterstonemf <waterstonemf@outlook.com>
2015-04-30 14:13:43 +0800

    AddAssertUtil

---
+++ CodeIgniter/AssetFolder/utility_helper.php
@@ -0,0 +1,17 @@
+<?php
+
+function asset_url(){
+   return base_url().'assets/';
+}
+
+function js_url(){
+   return base_url().'assets/js/';
+}
+
+function css_url(){
+   return base_url().'assets/css/';
+}
+
+function img_url(){
+   return base_url().'assets/img/';
+}
